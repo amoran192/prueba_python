8c44cf14e787eaa15d23595df76ac264f659666a
Alejandro Moran <amoranfer@gmail.com>
2021-08-23 20:09:18 +0200

    Version 3.0

---
+++ dict_comprehesion.py
@@ -0,0 +1,17 @@
+def run ():
+#    my_dict= {}
+#
+#   for i in range (1,101):
+ #       if i%3 !=0:
+  #          my_dict [i]= i**3
+
+   # my_dict = {i: i**3 for i in range (1,101) if i % 3 !=0 }
+
+    #print (my_dict)
+
+    challenge = {i: i**0.5 for i in range (1,101)}
+    print (challenge)
+
+
+if __name__ =="__main__":
+    run ()
