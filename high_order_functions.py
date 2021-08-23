3e768c1c0baccedd5cf8f8ddea21140c1bd3ba34
Alejandro Moran <amoranfer@gmail.com>
2021-08-24 00:53:08 +0200

    version 0,6

--- high_order_functions.py
+++ high_order_functions.py
@@ -1,8 +1,8 @@
-def run ():
+def run ():    
     my_list = [1,2,3,4,5,6,7,8,9]
     #odd = list(filter(lambda x: x%2 !=0, my_list))
-    odd=list(map(lambda x: x**2, my_list))
-
+    #odd=list(map(lambda x: x**2, my_list))
+    list()
     print (odd)
 
 
